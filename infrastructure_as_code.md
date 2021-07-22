39b925513b15170b1bd62e7b8f9d456a4daec281
Abdul Razak <razak8469@gmail.com>
2021-07-22 10:08:57 -0700

    creating resources

---
+++ infrastructure_as_code.md
@@ -0,0 +1,16 @@
+##### initialization
+	terraform init 
+
+##### creating resources
+	terraform apply
+
+##### check
+	if VPC network has been provisioned 
+
+##### inspect current state
+	terraform show
+
+
+
+
+
---
+++ infrastructure_as_code.tf
@@ -0,0 +1,22 @@
+terraform {
+  require_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+
+
